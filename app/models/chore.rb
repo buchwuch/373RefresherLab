a52e292dbf4cd09d0ac8add0cc746ccf254e57d0
Matthew Buchman <macpro+mbuchman-cmu@users.noreply.github.com>
2021-02-04 17:26:44 -0500

    chores pass all tests

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,22 @@
 class Chore < ApplicationRecord
     belongs_to :child
     belongs_to :task
+
+    validates_timeliness_of :due_on
+
+    scope :by_task, -> { joins(:task).order("name") }
+    scope :chronological, -> { order(:due_on) }
+    scope :pending, -> { where(completed: false) }
+    scope :done, -> { where(completed: true) }
+    scope :upcoming, -> { where("due_on >= ?", Date.current) }
+    scope :past, -> { where("due_on < ?", Date.current) }
+
+    def status
+        if self.completed
+            "Completed"  
+        else
+            "Pending"
+        end
+    end
+
 end
