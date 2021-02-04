2fd25f7ae80633c087da8d3c7d192dfa5d7cf4d4
Matthew Buchman <macpro+mbuchman-cmu@users.noreply.github.com>
2021-02-04 17:04:51 -0500

    added scopes + name method to child

--- app/models/child.rb
+++ app/models/child.rb
@@ -3,5 +3,12 @@ class Child < ApplicationRecord
     has_many :tasks, through: :chores
 
     validates_presence_of :first_name, :last_name
+
+    scope :alphabetical, -> { order(:last_name, :first_name) }
+    scope :active, -> { where(active: true) }
+
+    def name
+        self.first_name + " " + self.last_name
+    end
     
 end
